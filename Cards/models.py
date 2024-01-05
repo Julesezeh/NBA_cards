e4f5dced830004e108765355e9bdac3ca6ca77ae
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-01-05 05:35:39 +0100

    card models

--- Cards/models.py
+++ Cards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    DOB = models.DateField()
+    height = models.FloatField()
+    country = models.CharField(max_lenth=100)
+
+
+class Stats(models.Model):
+    three_pt = models.FloatField()
